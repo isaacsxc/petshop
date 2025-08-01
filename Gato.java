141c14e507f5cece31f37efcf76eeccf358f0129
Isaac <isaac.franca4807692@edu.pe.senac.br>
2025-08-01 07:55:52 -0300

    Atualização

--- Gato.java
+++ Gato.java
@@ -1,6 +1,5 @@
 public class Gato extends Animal {
-    // Atributo ehIndependente removido
-    // Construtor simplificado
+
 
     public Gato(String nome, int idade, String raca, String dono) {
         super(nome, idade, raca, dono);
